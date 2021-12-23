2822b29418e43a8203eb0b919c30e6f03eaee81e
ProgramLikeABeast <xuyang000111@gmail.com>
2021-12-22 23:54:18 -0500

    update a version of Tailwind usage with React

---
+++ workcation/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hero from "./Hero";
+import "./css/input.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Hero />, rootElement);
